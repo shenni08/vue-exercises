0d6ab2e0bbe84ba73f5a3ef90df24a09b5fa0489
shenni08 <madaicastro08@gmail.com>
2024-08-15 18:41:11 -0600

    refactor: update counter button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { FwbButton } from 'flowbite-vue'
 
 defineProps<{ msg: string }>()
 
@@ -10,7 +11,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <fwb-button gradient="green-blue" @click="count++">count is {{ count }}</fwb-button>
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
@@ -19,15 +20,12 @@ const count = ref(0)
 
   <p>
     Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the
+    official Vue + Vite starter
   </p>
   <p>
     Learn more about IDE Support for Vue in the
-    <a
-      href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support"
-      target="_blank"
+    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank"
       >Vue Docs Scaling up Guide</a
     >.
   </p>
